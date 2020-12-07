e230b43dbe1fba186a982f55eebbca013c717d1e
MickWerf <55532589+MickWerf@users.noreply.github.com>
2020-12-07 09:48:25 +0100

    Add files via upload

---
+++ app/src/main/java/com/mickwerf/digi_tours_breda/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mickwerf.digi_tours_breda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
