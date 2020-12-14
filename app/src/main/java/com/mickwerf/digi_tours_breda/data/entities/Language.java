c75e5b088d95f3d3cb3e28501c2550e4997108d3
MickWerf <mickwerf@outlook.com>
2020-12-14 09:51:47 +0100

    Added Room dependency Added Language entity example

--- app/src/main/java/com/mickwerf/digi_tours_breda/data/entities/Language.java
+++ app/src/main/java/com/mickwerf/digi_tours_breda/data/entities/Language.java
@@ -1,4 +1,23 @@
 package com.mickwerf.digi_tours_breda.data.entities;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Language {
+    @PrimaryKey
+    private String name;
+
+    public Language(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
