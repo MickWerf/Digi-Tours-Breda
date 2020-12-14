f1ea90fe91825e1f60cf5e2383cb23ce97e0030d
Wout <w.geuze@student.avans.nl>
2020-12-14 13:18:02 +0100

    Added map

--- app/src/main/java/com/mickwerf/digi_tours_breda/gui/fragments/MapScreenFragment.java
+++ app/src/main/java/com/mickwerf/digi_tours_breda/gui/fragments/MapScreenFragment.java
@@ -1,14 +1,28 @@
 package com.mickwerf.digi_tours_breda.gui.fragments;
 
+import android.Manifest;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mickwerf.digi_tours_breda.R;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +30,13 @@ import com.mickwerf.digi_tours_breda.R;
  * create an instance of this fragment.
  */
 public class MapScreenFragment extends Fragment {
+
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+
+    private MapView mapView;
+
+    private MyLocationNewOverlay locationOverlay;
+    private MapController mapController;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -54,6 +75,56 @@ public class MapScreenFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        Configuration.getInstance().load(getActivity().getApplication(), PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext()));
+
+        this.mapView = getActivity().findViewById(R.id.map_view);
+        this.mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
+        this.mapView.setMultiTouchControls(true);
+        this.mapView.setTileSource(TileSourceFactory.MAPNIK);
+
+        requestPermissions(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        }, REQUEST_PERMISSIONS_REQUEST_CODE);
+
+        this.locationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getActivity().getApplicationContext()), this.mapView);
+        this.locationOverlay.enableMyLocation();
+        this.locationOverlay.enableFollowLocation();
+        this.mapView.getOverlays().add(this.locationOverlay);
+        this.mapController = new MapController(this.mapView);
+        this.mapController.zoomTo(19);
+        this.mapController.setCenter(locationOverlay.getMyLocation());
+        this.mapController.animateTo(locationOverlay.getMyLocation());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        this.mapView.onResume();
+        this.locationOverlay.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        this.mapView.onPause();
+        this.locationOverlay.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (int i = 0; i < grantResults.length; i++) {
+            permissionsToRequest.add(permissions[i]);
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this.getActivity(),
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
         }
     }
 
