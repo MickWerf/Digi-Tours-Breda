7af60147f64a0beb39620428bb9f22f9a785271a
MickWerf <mickwerf@outlook.com>
2020-12-14 11:27:14 +0100

    Added Database Singleton

--- app/src/main/java/com/mickwerf/digi_tours_breda/data/Database.java
+++ app/src/main/java/com/mickwerf/digi_tours_breda/data/Database.java
@@ -1,4 +1,42 @@
 package com.mickwerf.digi_tours_breda.data;
 
-public class Database {
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.mickwerf.digi_tours_breda.data.entities.DataElement;
+import com.mickwerf.digi_tours_breda.data.entities.GpsCoordinate;
+import com.mickwerf.digi_tours_breda.data.entities.Language;
+import com.mickwerf.digi_tours_breda.data.entities.Location;
+import com.mickwerf.digi_tours_breda.data.entities.Route;
+import com.mickwerf.digi_tours_breda.data.entities.RouteLocationCrossReference;
+import com.mickwerf.digi_tours_breda.data.entities.UserSettings;
+
+@androidx.room.Database(
+        entities = {
+                DataElement.class,
+                GpsCoordinate.class,
+                Language.class,
+                Location.class,
+                Route.class,
+                RouteLocationCrossReference.class,
+                UserSettings.class
+        },
+        version = 1
+)
+public abstract class Database extends RoomDatabase {
+    private static volatile Database INSTANCE;
+    public abstract UserDataAccess userDataAccess();
+
+    public static Database getInstance(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (Database.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), Database.class, "digi_tours_breda").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
