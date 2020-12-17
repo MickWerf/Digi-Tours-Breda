25274efbd342758311f8b0b2d5711d9e3939b1ae
Samir <samir.rademakers3@gmail.com>
2020-12-17 10:21:28 +0100

    [Update] setttings screen explanations updated

--- app/src/main/java/com/mickwerf/digi_tours_breda/gui/fragments/SettingScreenFragment.java
+++ app/src/main/java/com/mickwerf/digi_tours_breda/gui/fragments/SettingScreenFragment.java
@@ -14,17 +14,9 @@ import android.widget.TextView;
 
 import com.mickwerf.digi_tours_breda.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingScreenFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SettingScreenFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     private ImageView howToUseIV;
     private ImageView noGpsIV;
@@ -32,68 +24,15 @@ public class SettingScreenFragment extends Fragment {
     private TextView howToUseTV;
     private TextView noGpsTV;
 
+    private TextView howToUseText;
+    private TextView noGpsText;
+
     private boolean howToUseOpen;
     private boolean noGpsOpen;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingScreenFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingScreenFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingScreenFragment newInstance(String param1, String param2) {
-        SettingScreenFragment fragment = new SettingScreenFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
-
-    public void showHowToUse(){
-        if(!this.howToUseOpen){
-            this.howToUseIV.setRotation(90);
-            this.howToUseTV.setVisibility(View.VISIBLE);
-            this.howToUseOpen = true;
-        }else {
-            this.howToUseIV.setRotation(0);
-            this.howToUseTV.setVisibility(View.GONE);
-            this.howToUseOpen = false;
-        }
-    }
-
-    public void showNoGpsCase(){
-        if(!this.noGpsOpen){
-            this.noGpsIV.setRotation(90);
-            this.noGpsTV.setVisibility(View.VISIBLE);
-            this.noGpsOpen = true;
-        }else {
-            this.noGpsIV.setRotation(0);
-            this.noGpsTV.setVisibility(View.GONE);
-            this.noGpsOpen = false;
-        }
-    }
+    private ImageView dutchButton;
+    private ImageView germanButton;
+    private ImageView englishButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -117,6 +56,19 @@ public class SettingScreenFragment extends Fragment {
         this.howToUseTV = this.getView().findViewById(R.id.HowToUseTV);
         this.noGpsTV = this.getView().findViewById(R.id.noGpsTV);
 
+        this.englishButton = this.getView().findViewById(R.id.englishButton);
+        this.dutchButton = this.getView().findViewById(R.id.dutchButton);
+        this.germanButton = this.getView().findViewById(R.id.germanButton);
+
+        this.noGpsText = this.getView().findViewById(R.id.noGpsText);
+        this.howToUseText = this.getView().findViewById(R.id.howToUseText);
+
+        setOnClickListeners();
+
+    }
+
+    public void setOnClickListeners(){
+
         this.howToUseIV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -130,5 +82,92 @@ public class SettingScreenFragment extends Fragment {
                 showNoGpsCase();
             }
         });
+
+        this.howToUseIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHowToUse();
+            }
+        });
+
+        this.noGpsIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNoGpsCase();
+            }
+        });
+
+        this.dutchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToDutch();
+            }
+        });
+
+        this.englishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToEnglish();
+            }
+        });
+
+        this.germanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToGerman();
+            }
+        });
+
+        this.howToUseText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHowToUse();
+            }
+        });
+
+        this.noGpsText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNoGpsCase();
+            }
+        });
+    }
+
+    public void changeToEnglish(){
+        //TODO change language to English
+    }
+
+    public void changeToDutch(){
+        //TODO change language to Dutch
+    }
+
+    public void changeToGerman(){
+        //TODO change language to German
+    }
+
+    public void showHowToUse(){
+        if(!this.howToUseOpen){
+            this.howToUseIV.setRotation(90);
+            this.howToUseTV.setVisibility(View.VISIBLE);
+            this.howToUseOpen = true;
+
+        }else {
+            this.howToUseIV.setRotation(0);
+            this.howToUseTV.setVisibility(View.GONE);
+            this.howToUseOpen = false;
+        }
+    }
+
+    public void showNoGpsCase(){
+        if(!this.noGpsOpen){
+            this.noGpsIV.setRotation(90);
+            this.noGpsTV.setVisibility(View.VISIBLE);
+            this.noGpsOpen = true;
+
+        }else {
+            this.noGpsIV.setRotation(0);
+            this.noGpsTV.setVisibility(View.GONE);
+            this.noGpsOpen = false;
+        }
     }
 }
