230c5282e42c21c4a5aed46dc2d0ea9b5d640d9c
Menno <mennobil02@gmail.com>
2021-01-04 14:36:23 +0100

    [FEATURE] all database integration tests added

--- app/src/androidTest/java/com/mickwerf/digi_tours_breda/data/DatabaseIntegrationTest.java
+++ app/src/androidTest/java/com/mickwerf/digi_tours_breda/data/DatabaseIntegrationTest.java
@@ -3,6 +3,9 @@ package com.mickwerf.digi_tours_breda.data;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.mickwerf.digi_tours_breda.data.entities.Route;
+import com.mickwerf.digi_tours_breda.data.entities.UserSettings;
+import com.mickwerf.digi_tours_breda.data.relations.LocationCoordinate;
+import com.mickwerf.digi_tours_breda.data.relations.LocationElements;
 import com.mickwerf.digi_tours_breda.data.relations.RouteWithLocations;
 
 import org.junit.Assert;
@@ -22,6 +25,7 @@ public class DatabaseIntegrationTest {
         this.dataAdminAccess = database.adminDataAccess();
     }
 
+    //New integration test (not in integration document)
     @Test
     public void testRoutes() {
         List<Route> routes = this.dataAdminAccess.getAllRoutes();
@@ -39,4 +43,42 @@ public class DatabaseIntegrationTest {
         Assert.assertEquals(!previousStatus, routeWithLocations.getRoute().isComplete());
     }
 
+    //Test I3.T1
+    @Test
+    public void testSettingLanguage() {
+        UserSettings oldUserSettings = this.dataAdminAccess.getUserSettings();
+
+        String newLanguage = "";
+        switch (oldUserSettings.getLanguage()) {
+            case "Nederlands":
+                newLanguage = "Engels";
+                break;
+            case "Engels":
+                newLanguage = "Nederlands";
+                break;
+        }
+
+        oldUserSettings.setLanguage(newLanguage);
+        this.dataAdminAccess.updateCurrentUserSettings(oldUserSettings);
+
+
+        UserSettings newUserSettings = this.dataAdminAccess.getUserSettings();
+        Assert.assertEquals(newLanguage, newUserSettings.getLanguage());
+    }
+
+    //Test I4.T1
+    @Test
+    public void testGetAllCoordinates() {
+        List<LocationCoordinate> locationCoordinates = this.dataAdminAccess.getLocationCoordinates();
+
+        Assert.assertEquals(30, locationCoordinates.size());
+    }
+
+    //Test I4.T2
+    @Test
+    public void testLocationElements() {
+        List<LocationElements> locationElements = this.dataAdminAccess.getLocationElementsFromLanguage("Avans Hogeschool", "Nederlands");
+
+        Assert.assertEquals(1, locationElements.size());
+    }
 }
