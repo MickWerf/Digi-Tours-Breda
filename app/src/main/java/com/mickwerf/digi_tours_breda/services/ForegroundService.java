84aa64172561e6c2b75008df6c17d56213bf569e
Timo <timojenkins21@gmail.com>
2021-01-06 18:01:20 +0100

    edits

--- app/src/main/java/com/mickwerf/digi_tours_breda/services/ForegroundService.java
+++ app/src/main/java/com/mickwerf/digi_tours_breda/services/ForegroundService.java
@@ -9,12 +9,15 @@ import android.content.Intent;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 import com.mickwerf.digi_tours_breda.R;
 import com.mickwerf.digi_tours_breda.gui.activities.MainActivity;
 
+import static com.mickwerf.digi_tours_breda.services.ServiceChannel.CHANNEL_ID;
+
 public class ForegroundService extends Service {
-    private static final int NOTIFY_ID = 1;
+    //private static final int NOTIFY_ID = 1;
 
     @Override
     public void onCreate() {
@@ -23,29 +26,42 @@ public class ForegroundService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        startForeground(NOTIFY_ID, getMyActivityNotification());
-        return super.onStartCommand(intent, flags, startId);
-    }
+        //startForeground(NOTIFY_ID, getMyActivityNotification());
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
 
-    private Notification getMyActivityNotification(){
-        PendingIntent contentIntent = PendingIntent.getActivity(this,
-                0, new Intent(this, MainActivity.class), 0);
-
-        return new Notification.Builder(this)
-                .setContentTitle("App")
-                .setContentText("test")
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("deze app")
+                .setContentText("tetst")
                 .setSmallIcon(R.drawable.logo)
-                .setContentIntent(contentIntent)
+                .setContentIntent(pendingIntent)
                 .build();
+
+        startForeground(1, notification);
+
+        return START_NOT_STICKY;
     }
 
-    private void updateNotification(){
-
-        Notification notification = getMyActivityNotification();
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(NOTIFY_ID, notification);
-    }
+//    private Notification getMyActivityNotification(){
+//        PendingIntent contentIntent = PendingIntent.getActivity(this,
+//                0, new Intent(this, MainActivity.class), 0);
+//
+//        return new Notification.Builder(this)
+//                .setContentTitle("App")
+//                .setContentText("test")
+//                .setSmallIcon(R.drawable.logo)
+//                .setContentIntent(contentIntent)
+//                .build();
+//    }
+//
+//    private void updateNotification(){
+//
+//        Notification notification = getMyActivityNotification();
+//
+//        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        notificationManager.notify(NOTIFY_ID, notification);
+//    }
 
     @Override
     public void onDestroy() {
